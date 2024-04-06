a9715c39f7d96d0aa2ba196e4fdca7f96aa255e2
ivansxs23 <dconverso@frba.utn.edu.ar>
2024-04-06 07:24:57 +0000

    creo que ya esta

--- client/main.go
+++ client/main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
 
 	// loggear "Hola soy un log" usando la biblioteca log
+	log.Println("tp0.log", "Hola soy un log")
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
 	// validar que la config este cargada correctamente
-
+	if globals.ClientConfig == nil {
+		log.Fatalf("No se pudo cargar la configuración")
+	} else {
+		log.Println("tp0.log", globals.ClientConfig)
+	}
 	// loggeamos el valor de la config
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// enviar un mensaje al servidor con el valor de la config
+	var clave string
+	utils.EnviarMensaje("ip", 8008, clave)
 
 	// leer de la consola el mensaje
-	// utils.LeerConsola()
+	utils.LeerConsola()
 
 	// generamos un paquete y lo enviamos al servidor
-	// utils.GenerarYEnviarPaquete()
+	utils.GenerarYEnviarPaquete()
 }
